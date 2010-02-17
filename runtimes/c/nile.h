b00c9a11e11541850f4c469a4f88b3277b6c2507
Dan Amelang <dan@amelang.net>
2010-02-17 15:28:02 -0800

    Remove nile_Real_mod
    This will be a user-defined operator.

--- runtimes/c/nile.h
+++ runtimes/c/nile.h
@@ -56,7 +56,6 @@ static inline real nile_Real_min (real a, real b) { return a < b ? a : b; }
 static inline real nile_Real_max (real a, real b) { return a > b ? a : b; }
 static inline real nile_Real_ave (real a, real b) { return (a + b) / 2; }
 static inline real nile_Real_sel (real a, real b, real c) { return b ? a : c; }
-static inline real nile_Real_mod (real a, real b) { return fmod (a, b); }
 
 /* Kernels */
 
