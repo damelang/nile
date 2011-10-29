fb80bd037c2ba096d53c03f56f1d3a3082d09e1f
Dan Amelang <dan@amelang.net>
2011-10-29 01:23:30 -0700

    Don't crash just because the wrong quantum was given.

--- runtimes/c/test/nile-print.h
+++ runtimes/c/test/nile-print.h
@@ -15,7 +15,7 @@ nile_PrintToFile_body (nile_Process_t *p, nile_Buffer_t *in, nile_Buffer_t *out)
     nile_PrintToFile_vars_t v = *(nile_PrintToFile_vars_t *) nile_Process_vars (p);
     while (!nile_Buffer_is_empty (in)) {
         int q = v.quantum;
-        while (q--)
+        while (q-- && !nile_Buffer_is_empty (in))
             fprintf (v.f, "%.4f ", nile_Real_tof (nile_Buffer_pop_head (in)));
         fprintf (v.f, "\n");
     }
